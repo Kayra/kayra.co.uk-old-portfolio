588a1e5a0bf54d8dc774f8c09e46220704fc912b
Kayra <KayraAlat@gmail.com>
2017-08-04 13:07:12 +0300

    Added mouseover for all header elements

--- js/main.js
+++ js/main.js
@@ -7,14 +7,6 @@
             $(this).children("span").toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
         });
 
-        $(".header h4, .header span, .header img").mouseover(function() {
-
-            var header_text = $(this).parent().siblings().find('h4');
-            var chevron_span = $(this).parent().siblings().find('span');
-            var img = $(this).parent().siblings().find('img');
-
-        });
-
    });
 
 }(window.jQuery, window, document));
