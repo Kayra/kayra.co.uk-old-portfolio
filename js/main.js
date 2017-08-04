0950e160cb340add8160ef229c013c92c13b7615
Kayra <KayraAlat@gmail.com>
2017-08-04 11:03:07 +0300

    Added mouseover for all header elements

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@
             $(this).children("span").toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
         });
 
+        $(".header h4, .header span, .header img").mouseover(function() {
+            console.log("hit");
+        });
+
    });
 
 }(window.jQuery, window, document));
