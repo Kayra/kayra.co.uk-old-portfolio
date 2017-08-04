2738151fd7a52e0f3301789ea46e16c0a5ea3ede
Kayra <KayraAlat@gmail.com>
2017-08-04 09:13:20 +0300

    Initialised file for jquery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$(function() {
+ (function($, window, document) {
 
-});
+   $(function() {
+
+     // The DOM is ready!
+
+   });
+
+   // The rest of the code goes here!
+
+}(window.jQuery, window, document));
